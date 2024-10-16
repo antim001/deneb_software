541b078b290f373c30eeb6625c54c7bcc9bb31f7
sakibul-huda <shuda594@gmail.com>
2024-10-17 00:33:41 +0600

    sidebar height and bg-colour fixed

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -48,7 +48,8 @@ const Sidebar = () => {
 
   return (
     <div className='flex'>
-      <div className={`bg-dark-purple h-full p-5 pt-8 ${open ? "w-72" : "w-20"} duration-300 relative`}>
+      
+      <div className={`min-h-screen  bg-dark-purple p-5 pt-8 ${open ? "w-72" : "w-20"} duration-300 relative`}>
         <BsArrowLeftShort
           className={`bg-white text-dark-purple text-3xl rounded-full absolute -right-3 top-9 border border-dark-purple cursor-pointer ${!open && 'rotate-180'}`}
           onClick={() => setOpen(!open)}
