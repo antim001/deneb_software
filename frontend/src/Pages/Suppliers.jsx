112b5facbe1e07ae302dfb171d7db278eb96b075
sakibul-huda <shuda594@gmail.com>
2024-10-16 13:16:33 +0600

    supplier added

---
+++ frontend/src/Pages/Suppliers.jsx
@@ -0,0 +1,13 @@
+
+
+const Suppliers = () => {
+    return (
+        <>
+            <div className="text-center text-5xl font-bold mt-12">
+                This is supplier page
+            </div>
+        </>
+    );
+};
+
+export default Suppliers;
--- frontend/src/Routes/Router.jsx
+++ frontend/src/Routes/Router.jsx
@@ -5,16 +5,19 @@ import Products from '../Pages/Products.jsx';
 import Account from '../Pages/Account.jsx';
 import Dashboard from '../Pages/Dashboard.jsx'
 import AddProduct from '../components/AddProduct.jsx'
+import Suppliers from "../Pages/Suppliers.jsx";
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />, // Render the Layout component
     children: [
+      { path: "/", element:<Dashboard/> },
         { path: "/allusers", element: <AllUsers /> }, // AllUsers component
         { path: "/allproducts", element: <Products /> },
         { path: "/addproduct", element: <AddProduct /> },
         { path: "/account", element: <Account /> },
-        {path:'/dashboard',element:<Dashboard/>}
+        {path:'/dashboard',element:<Dashboard/>},
+        {path:'/suppliers',element:<Suppliers/>}
       ],
   },
 ]);
