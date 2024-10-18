bdcadce50d202097f5a9ee65f063769f9290c4d9
sakibul-huda <shuda594@gmail.com>
2024-10-18 16:23:16 +0600

    sidebar menu bg color added when active

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -5,8 +5,10 @@ import { FaUsersViewfinder } from "react-icons/fa6";
 import { useState } from 'react';
 import { AiFillEnvironment } from 'react-icons/ai';
 import { RiDashboardFill } from 'react-icons/ri';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import { VscPersonAdd } from "react-icons/vsc";
+import './CSS/Sidebar.css'
+
 const Sidebar = () => {
   const [open, setOpen] = useState(true);
   
@@ -49,7 +51,7 @@ const Sidebar = () => {
   ];
 
   return (
-    <div className='flex'>
+    <div className='flex isMenuActive '>
       
       <div className={`min-h-screen  bg-dark-purple p-5 pt-8 ${open ? "w-72" : "w-20"} duration-300 relative`}>
         <BsArrowLeftShort
@@ -73,20 +75,24 @@ const Sidebar = () => {
           {Menus.map((menu, index) => (
             <div key={index}>
               <li
-                className={`text-gray-300 text-sm flex items-center gap-x-4 cursor-pointer p-2 hover:bg-light-white rounded-md ${menu.spacing ? "mt-9" : "mt-2"}`}
-              >
+                className={` text-gray-300 text-sm flex items-center gap-x-4 cursor-pointer p-2 hover:bg-light-white rounded-md ${menu.spacing ? "mt-9" : "mt-2"}`}
+                
+                onClick={() => menu.setSubmenuOpen(!menu.submenuOpen)}
+              > 
+              <NavLink to={menu.path} className={`flex items-center gap-x-4 text-base font-medium flex-1 ${!open && "hidden"}`}>
+
                 <span className='text-2xl block float-left'>
                   {menu.icon ? menu.icon : <RiDashboardFill />}
                 </span>
-                <Link to={menu.path} className={`text-base font-medium flex-1 ${!open && "hidden"}`}
-                onClick={() => menu.setSubmenuOpen(!menu.submenuOpen)}>
+               
                   {menu.title}
-                </Link>
+                </NavLink>
+
                 {menu.submenu && open && (
                   <BsChevronDown
                     
                     className={`${menu.submenuOpen ? "rotate-180 duration-300" : ""}`}
-                    onClick={() => menu.setSubmenuOpen(!menu.submenuOpen)}
+                    
                   />
                 )}
               </li>
@@ -97,9 +103,9 @@ const Sidebar = () => {
                   {menu.submenuItems.map((subMenuItem, subIndex) => (
                     <li
                       key={subIndex}
-                      className='text-gray-300 text-sm flex items-center gap-x-4 cursor-pointer p-2 px-5 hover:bg-light-white rounded-md'
+                      className=' text-gray-300 text-sm flex items-center gap-x-4 cursor-pointer p-2 px-5 hover:bg-light-white rounded-md'
                     >
-                      <Link to={subMenuItem.path}>{subMenuItem.title}</Link>
+                      <NavLink to={subMenuItem.path}>{subMenuItem.title}</NavLink>
                     </li>
                   ))}
                 </ul>
